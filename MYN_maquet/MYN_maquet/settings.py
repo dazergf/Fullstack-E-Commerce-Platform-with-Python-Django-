40088761e9c8db65d75bcc5fbb526e1763d62d18
dazergf <dazergf@gmail.com>
2025-05-08 14:42:34 +0100

    portofolio

--- MYN_maquet/MYN_maquet/settings.py
+++ MYN_maquet/MYN_maquet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a2=vs7l#qmg=51lm(lz*p5t7vf)qgs$hk@)um+0hd-(m&oev5-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
